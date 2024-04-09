5ea6a09a6c79e38705e28e7a14a63403e6092ef7
u220619 <andreas.bomonti@sbb.ch>
2024-04-09 09:07:33 +0200

    Correction

--- emate_mock_fusion/scripts/mock_fusion_node.py
+++ emate_mock_fusion/scripts/mock_fusion_node.py
@@ -26,7 +26,8 @@ class MockFusion:
             queue_size=10,
         )
 
-    def _timer_callback(self):
+    def _timer_callback(self, event):
+        print 'Timer called at ' + str(event.current_real)
         self._write_msgs()
 
     def _gnss_callback(self, data: GnssPvt):
